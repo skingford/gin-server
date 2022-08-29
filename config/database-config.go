5233a693537f4b9410137a9d6be377503a75f163
kingford <32887862+skingford@users.noreply.github.com>
2022-08-29 17:03:38 +0800

    feat: :tada: init

---
+++ config/database-config.go
@@ -0,0 +1,54 @@
+package config
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/skingford/gin-server/entity"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+// SetupDatabaseConnection is creating a new connection to our database
+func SetupDatabaseConnection() *gorm.DB {
+	errEnv := godotenv.Load()
+	if errEnv != nil {
+		panic("Failed to load env file")
+	}
+
+	dbUser := os.Getenv("DB_USER")
+	dbPass := os.Getenv("DB_PASS")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbName := os.Getenv("DB_NAME")
+
+	// dsn := "host=localhost user=gorm password=gorm dbname=gorm port=9920 sslmode=disable TimeZone=Asia/Shanghai"
+	//dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", dbUser, dbPass, dbHost, dbPort, dbName)
+
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Shanghai", dbHost, dbUser, dbPass, dbName, dbPort)
+
+	fmt.Println("dsn:" + dsn)
+
+	db, err := gorm.Open(postgres.New(postgres.Config{
+		DSN:                  dsn,
+		PreferSimpleProtocol: true, // disables implicit prepared statement usage
+	}), &gorm.Config{})
+
+	if err != nil {
+		panic("Failed to create a connection to database")
+	}
+
+	//nanti kita isi modelnya di sini
+	db.AutoMigrate(&entity.Book{}, &entity.User{})
+	return db
+}
+
+// CloseDatabaseConnection method is closing a connection between your app and your db
+func CloseDatabaseConnection(db *gorm.DB) {
+	dbSQL, err := db.DB()
+	if err != nil {
+		panic("Failed to close connection from database")
+	}
+	dbSQL.Close()
+}
